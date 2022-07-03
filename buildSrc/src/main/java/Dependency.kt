f383ec7d8348020877df029fc64671ea2fe82d8b
Anbyeonghun <abh0920one@gmail.com>
2022-07-03 18:11:25 +0900

    #3 Set Dependency object 추가

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,2 +1,24 @@
 object Dependency {
+    object AndroidX {
+        const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+        const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT}"
+        const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
+        const val LIVE_DATA = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.LIVE_DATA}"
+        const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.VIEWMODEL}"
+        const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:${Versions.NAVIGATION_FRAGMENT}"
+        const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:${Versions.NAVIGATION_UI}"
+    }
+
+    object Google{
+        const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+    }
+
+    object UnitTest {
+        const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    }
+
+    object AndroidTest {
+        const val ANDROID_JUNIT = "androidx.test.ext:junit:${Versions.JUNIT}"
+        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO}"
+    }
 }
