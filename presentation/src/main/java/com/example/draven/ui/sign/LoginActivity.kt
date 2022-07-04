cc95e3bfba742c3e318b969a7cd932442d6ad664
Anbyeonghun <abh0920one@gmail.com>
2022-07-04 15:51:58 +0900

    #7 Feature login activity base activity extend

--- presentation/src/main/java/com/example/draven/ui/sign/LoginActivity.kt
+++ presentation/src/main/java/com/example/draven/ui/sign/LoginActivity.kt
@@ -1,12 +1,13 @@
 package com.example.draven.ui.sign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.draven.R
+import com.example.draven.base.BaseActivity
+import com.example.draven.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
     }
 }
