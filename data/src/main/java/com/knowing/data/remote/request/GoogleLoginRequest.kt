fbb0c672238fd8726ae0fac59e2071f3a9e30714
Anbyeonghun <abh0920one@gmail.com>
2022-07-05 14:52:10 +0900

    #7 Feature google login entity data class

--- data/src/main/java/com/knowing/data/remote/request/GoogleLoginRequest.kt
+++ data/src/main/java/com/knowing/data/remote/request/GoogleLoginRequest.kt
@@ -1,3 +1,7 @@
 package com.knowing.data.remote.request
 
-data class GoogleLoginRequest()
+data class GoogleLoginRequest(
+    val name: String,
+    val email: String,
+    val picture: String?
+)
--- data/src/main/java/com/knowing/data/remote/response/GoogleLoginResponse.kt
+++ data/src/main/java/com/knowing/data/remote/response/GoogleLoginResponse.kt
@@ -1,3 +1,7 @@
 package com.knowing.data.remote.response
 
-data class GoogleLoginResponse()
+data class GoogleLoginResponse(
+    val access_token: String,
+    val refresh_token: String,
+    val is_first_login: Boolean
+)
