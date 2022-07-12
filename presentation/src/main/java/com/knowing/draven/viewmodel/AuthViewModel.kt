c2b4d16fa7080b1aae36737088a11a13c7cbadda
Anbyeonghun <abh0920one@gmail.com>
2022-07-12 11:13:01 +0900

    #7 Add auth viewmodel

---
+++ presentation/src/main/java/com/knowing/draven/viewmodel/AuthViewModel.kt
@@ -0,0 +1,9 @@
+package com.knowing.draven.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class AuthViewModel : ViewModel(){
+    
+}
