ebfea31e858b22c509061045bdecf8b76f658c40
Anbyeonghun <abh0920one@gmail.com>
2022-07-12 14:19:50 +0900

    #7 Feature auth viewmodel implement google login

--- presentation/src/main/java/com/knowing/draven/viewmodel/AuthViewModel.kt
+++ presentation/src/main/java/com/knowing/draven/viewmodel/AuthViewModel.kt
@@ -1,9 +1,53 @@
 package com.knowing.draven.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.knowing.domain.base.BadRequest
+import com.knowing.domain.base.UnAuthorized
+import com.knowing.domain.usecase.auth.FetchGoogleSignInUseCase
+import com.knowing.draven.util.MutableEventFlow
+import com.knowing.draven.util.asEventFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class AuthViewModel : ViewModel(){
-    
+class AuthViewModel @Inject constructor(
+    private val fetchGoogleSignInUseCase: FetchGoogleSignInUseCase
+) : ViewModel() {
+
+    private val _eventFlow = MutableEventFlow<Event>()
+    val eventFlow = _eventFlow.asEventFlow()
+
+    private fun event(event: Event) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
+
+    fun googleSignIn(code: String) {
+        viewModelScope.launch {
+            val authCode = HashMap<String, String>()
+            authCode["code"] = code
+            kotlin.runCatching {
+                fetchGoogleSignInUseCase.invoke(authCode)
+            }.onSuccess {
+                event(Event.SuccessGoogleSignIn(true))
+            }.onFailure {
+                httpExceptionHandler(throwable = it)
+            }
+        }
+    }
+
+    private fun httpExceptionHandler(throwable: Throwable) =
+        when(throwable){
+            is BadRequest -> { event(Event.ErrorMessage("")) }
+            is UnAuthorized -> { event(Event.ErrorMessage("")) }
+            else -> {}
+        }
+
+    sealed class Event {
+        data class SuccessGoogleSignIn(var state: Boolean = false) : Event()
+        data class ErrorMessage(val errorMessage: String) : Event()
+    }
 }
